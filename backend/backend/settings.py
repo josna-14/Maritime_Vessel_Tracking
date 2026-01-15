5112ded2ecf737a24cb73ac18cfa74422b7ac239
josna <s.josna14@gmail.com>
2026-01-15 13:07:06 +0530

    Hard reset of migrations to fix missing tables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,7 @@ MIDDLEWARE = [
     'core.middleware.RequestThroughputMiddleware',
     'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -40,7 +41,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
